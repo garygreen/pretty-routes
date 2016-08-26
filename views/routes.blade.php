f8723dfadad72342f13f91e5392d5ae7c62dceda
Gary Green <holegary@gmail.com>
2016-08-26 15:11:07 +0100

    Add doctype

--- views/routes.blade.php
+++ views/routes.blade.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
