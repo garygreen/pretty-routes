ff20fe825f9fb883a18d81ee8f6e3fdcd1eba9e2
Deejavu <DeeJaVu@users.noreply.github.com>
2016-09-02 18:43:27 +0300

    Removed Bootstrap.js script
    We don't need bootystrap.js script

--- views/routes.blade.php
+++ views/routes.blade.php
@@ -61,6 +61,5 @@
         </tbody>
     </table>
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
 </body>
 </html>
