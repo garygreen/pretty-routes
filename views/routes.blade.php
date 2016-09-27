12312d7c78004070991fcb8cf10fce699338b045
Lenyn Alcántara <nightzpy@gmail.com>
2016-09-27 13:52:59 -0300

    Datatables was add to project

--- views/routes.blade.php
+++ views/routes.blade.php
@@ -2,6 +2,19 @@
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/colreorder/1.3.2/css/colReorder.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/fixedheader/3.1.2/css/fixedHeader.bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/colreorder/1.3.2/js/dataTables.colReorder.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/fixedheader/3.1.2/js/dataTables.fixedHeader.min.js"></script>    
+
     <style type="text/css">
         body {
             padding: 60px;
@@ -61,5 +74,18 @@
         </tbody>
     </table>
 
+    <script>
+        $(document).ready(function() {
+             $(document).ready(function () {
+                 var table = $('.table').DataTable( {
+                           "pageLength": 5,
+                            "pagingType": "full_numbers",
+                            colReorder: true,
+                            fixedHeader: true
+                      });
+                    $('.table').removeClass( 'display' ).addClass('table table-striped table-bordered');
+                });
+        } );
+    </script>
 </body>
 </html>
