907ea0a054ec6572a311562120c2535f325706c6
Andrey Helldar <andrey-helldar@users.noreply.github.com>
2020-10-06 09:57:57 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ Route::name('pretty-routes.')
     ->middleware(config('pretty-routes.middlewares'))
     ->prefix(config('pretty-routes.url'))
     ->group(function () {
-
         Route::get('/', 'PrettyRoutes\Http\PrettyRoutesController@show')->name('show');
         Route::get('json', 'PrettyRoutes\Http\PrettyRoutesController@routes')->name('list');
-
     });
