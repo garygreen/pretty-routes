bf75134f7a7548dbeee81af315e8c4403e920057
Andrey Helldar <helldar@ai-rus.com>
2020-10-22 19:47:27 +0300

    Added tests

--- tests/TranslationsTest.php
+++ tests/TranslationsTest.php
@@ -13,10 +13,30 @@ final class TranslationsTest extends TestCase
         $response = $this->get('/routes');
 
         $response->assertStatus(200);
-        $response->assertSee('Routes');
-        $response->assertSee('Middlewares');
+
+        $response->assertSee('Action');
+        $response->assertSee('All');
+        $response->assertSee('Deprecated');
+        $response->assertSee('Domain');
         $response->assertSee('Loading... Please wait...');
+        $response->assertSee('Methods');
+        $response->assertSee('Middlewares');
+        $response->assertSee('Module');
+        $response->assertSee('Name');
+        $response->assertSee('No data available');
+        $response->assertSee('No matching records found');
+        $response->assertSee('of');
+        $response->assertSee('Only');
+        $response->assertSee('Open the project page on GitHub');
+        $response->assertSee('Path');
+        $response->assertSee('Priority');
+        $response->assertSee('Refresh the list of routes');
         $response->assertSee('Routes per page');
+        $response->assertSee('Routes');
+        $response->assertSee('Search');
+        $response->assertSee('Show');
+        $response->assertSee('Without');
+        $response->assertSee('{0}-{1} of {2}');
 
         $response->assertDontSee('Foo Bar');
         $response->assertDontSee('Записей на странице');
@@ -29,10 +49,30 @@ final class TranslationsTest extends TestCase
         $response = $this->get('/routes');
 
         $response->assertStatus(200);
-        $response->assertSee('Маршруты');
-        $response->assertSee('Мидлвари');
+
+        $response->assertSee('URI');
+        $response->assertSee('{0}-{1} из {2}');
+        $response->assertSee('Все');
+        $response->assertSee('Данные отсутствуют');
+        $response->assertSee('Домен');
         $response->assertSee('Загрузка... Пожалуйста, подождите...');
         $response->assertSee('Записей на странице');
+        $response->assertSee('из');
+        $response->assertSee('Маршруты');
+        $response->assertSee('Методы');
+        $response->assertSee('Мидлвари');
+        $response->assertSee('Модуль');
+        $response->assertSee('Название');
+        $response->assertSee('Не указано');
+        $response->assertSee('Обновить список маршрутов');
+        $response->assertSee('Открыть страницу проекта в GitHub');
+        $response->assertSee('Поиск');
+        $response->assertSee('Показать');
+        $response->assertSee('Приоритет');
+        $response->assertSee('Только');
+        $response->assertSee('Устаревшие');
+        $response->assertSee('Экшены');
+        $response->assertSee('Элементы не найдены');
 
         $response->assertDontSee('Foo Bar');
         $response->assertDontSee('Routes per page');
