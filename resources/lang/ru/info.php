89361dab6ce80193ec60fc79b1c5e47d3a36baf6
Andrey Helldar <helldar@ai-rus.com>
2021-07-07 00:26:12 +0300

    Added copyrights

--- resources/lang/ru/info.php
+++ resources/lang/ru/info.php
@@ -1,4 +1,19 @@
 <?php
+/******************************************************************************
+ * This file is part of the "andrey-helldar/pretty-routes" project.           *
+ *                                                                            *
+ * @author Andrey Helldar <helldar@ai-rus.com>                                *
+ * @author Gary Green <holegary@gmail.com>                                    *
+ *                                                                            *
+ * @copyright 2021 Andrey Helldar, Gary Green                                 *
+ *                                                                            *
+ * @license MIT                                                               *
+ *                                                                            *
+ * @see https://github.com/andrey-helldar/pretty-routes                       *
+ *                                                                            *
+ * For the full copyright and license information, please view the LICENSE    *
+ * file that was distributed with this source code.                           *
+ ******************************************************************************/
 
 return [
     'action'              => 'Экшены',
